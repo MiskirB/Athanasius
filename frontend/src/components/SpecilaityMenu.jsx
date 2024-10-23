37a2e34117b59bc08510c9531e98e3b3a3b79f08
MiskirB <156695703+MiskirB@users.noreply.github.com>
2024-10-23 23:25:39 +0300

    spelling speciality

--- frontend/src/components/SpecilaityMenu.jsx
+++ frontend/src/components/SpecilaityMenu.jsx
@@ -6,7 +6,7 @@ const SpecilaityMenu = () => {
   return (
     <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='#speciality '>
 <h1 className='text-3xl font-medium'> 
-    Find by Specilaity
+    Find by Speciality
 </h1>
 <p className='sm:w-1/3 text-center text-sm '>
 Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.
